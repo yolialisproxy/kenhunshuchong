2c222659b9b6c562251b925305f14252424cf641
yolialisproxy <160247077+yolialisproxy@users.noreply.github.com>
2025-08-18 15:50:59 +0800

    Update comments.js

--- api/comments.js
+++ api/comments.js
@@ -8,18 +8,18 @@ const cors = Cors({ origin: true });
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      type: process.env.FIREBASE_TYPE,
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI,
-      token_uri: process.env.FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+      type: process.FIREBASE_TYPE,
+      project_id: process.FIREBASE_PROJECT_ID,
+      private_key_id: process.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.FIREBASE_CLIENT_EMAIL,
+      client_id: process.FIREBASE_CLIENT_ID,
+      auth_uri: process.FIREBASE_AUTH_URI,
+      token_uri: process.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.FIREBASE_AUTH_PROVIDER_CERT_URL,
+      client_x509_cert_url: process.FIREBASE_CLIENT_CERT_URL
     }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    databaseURL: process.FIREBASE_DATABASE_URL
   });
 }
 
