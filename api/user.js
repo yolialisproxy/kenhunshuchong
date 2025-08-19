34b9e1193bfc524b9d1e7a647e1039f310612395
yolialisproxy <160247077+yolialisproxy@users.noreply.github.com>
2025-08-19 19:42:21 +0800

    Update user.js

--- api/user.js
+++ api/user.js
@@ -6,12 +6,13 @@ import { getDatabase, ref, get, set } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
