77b78f26ac259fb2ed618462fd2fb2e9b6be400a
yolialisproxy <160247077+yolialisproxy@users.noreply.github.com>
2025-08-19 14:40:39 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
-import { submitComment, getComments, deleteComment, editComment } from "./comments";
+import { submitComment, getComments, deleteComment, editComment, likeComment } from "./comments"; // 注意引入 likeComment
 
-// 处理请求
 export default async function handler(req, res) {
+  // POST 同时处理主评论和点赞区分
   if (req.method === "POST") {
-    return submitComment(req, res);
+    // 如果请求 body 有 commentId 且没有 name/email/comment，则视为点赞
+    if (req.body.commentId && !req.body.comment) {
+      return likeComment(req, res);
+    } else {
+      return submitComment(req, res);
+    }
   } else if (req.method === "GET") {
     return getComments(req, res);
   } else if (req.method === "DELETE") {
