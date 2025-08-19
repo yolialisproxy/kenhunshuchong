8d25b3b607ec73deb6d4b7030f3e643cb178d6e4
yolialisproxy <160247077+yolialisproxy@users.noreply.github.com>
2025-08-19 19:19:44 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") return res.status(200).end();
+  
 
   try {
     // 支持 action 放在 query 或者 body 中
