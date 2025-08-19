56899c3bf379b91e89525bef679bcff3d9ff70c4
yolialisproxy <yolialisproxy@gmail.com>
2025-08-19 22:38:23 +0800

    fix

--- api/index.js
+++ api/index.js
@@ -5,7 +5,11 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") return res.status(200).end();
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
 
   try {
     const action = req.query.action;
