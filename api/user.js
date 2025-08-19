065baffe4408b6b34afb8417d0dc71118db82ec0
yolialisproxy <160247077+yolialisproxy@users.noreply.github.com>
2025-08-19 21:24:03 +0800

    Update user.js

--- api/user.js
+++ api/user.js
@@ -22,7 +22,8 @@ export async function registerUserHandler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  
+
+  alert"dsfjdsklfj";
   try {
     const { username, email, password } = req.body;
 
