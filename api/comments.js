3a6a4805d55f7f6c7336990e2660a4402b485942
yolialisproxy <yolialisproxy@gmail.com>
2025-08-21 13:27:37 +0800

    fix

--- api/comments.js
+++ api/comments.js
@@ -84,7 +84,7 @@ export async function getComments(req, res) {
   setCORS(res);
 
   const body = await parseBody(req);
-  const { postId } = body || req.query;
+  const { postId } = req.query || body;
   if (!postId) return res.status(400).json({ error: '缺少 postId 参数' });
 
   try {
