1ea9697c245a04c88bb81ae60f1d29d4bf757fe8
yolialisproxy <yolialisproxy@gmail.com>
2025-08-21 12:50:43 +0800

    fix

--- api/comments.js
+++ api/comments.js
@@ -83,7 +83,7 @@ export async function submitComment(req, res) {
 export async function getComments(req, res) {
   setCORS(res);
 
-  const { postId } = await parseBody(req);
+  const { postId } = await parseBody(req) || req.query.postId;
   if (!postId) return res.status(400).json({ error: '缺少 postId 参数' });
 
   try {
