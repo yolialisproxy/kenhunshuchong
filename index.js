5f16e8d58a1c721d640d51bdc4c93319829d2bb7
yolialisproxy <yolialisproxy@gmail.com>
2025-08-23 16:52:41 +0800

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = async function handler(req, res) {
   res.status(200).json({
     message: "Custom Comment System API running",
-    routes: ["/comments", "/likes", "/", "/index", "/users"]
+    routes: ["/", "/index"]
   });
 };
